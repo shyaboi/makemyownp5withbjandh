8da86d6df241bcc300d4bd725a04d35740a5b07e
Shyaboi <ian.shane.sears@gmail.com>
2020-11-16 23:54:53 -0700

    took out logz

--- sketch.js
+++ sketch.js
@@ -13,30 +13,40 @@ function pauseAudio() {
 function skipAudio() {
   x.currentTime +=20
 }
+  const ok = async (dur)=> {
+    if (x.networkState=1) {
+      dur = await x.duration;
+    }
+  }
+
+  
+ok()
+
+for (const file of object) {
+  
+}
 
 const dinus = () => {
   setInterval(() => {
   cTime = parseInt(x.currentTime);
   // cTime = x.currentTime;
-  console.log(cTime)
+  // console.log(cTime)
   if(cTime===0){return}
   if (cTime % 3===0&&cTime%5===0) {
-    console.log(cTime+'boooooooooth');
+    // console.log(cTime+'boooooooooth');
     document.querySelector("html").style.background =
       "url('assets/me.png')";
     return;
   }
   if (cTime % 3===0) {
-    console.log('only 3 '+cTime);
+    // console.log('only 3 '+cTime);
     document.querySelector("html").style.background =
       "url('assets/bffpy.jpg')";
     return;
   }
 
   if (cTime % 5===0) {
-    console.log('only 5 '+cTime);
-
-    // alert(`this many thing  ${cTime}`)
+    // console.log('only 5 '+cTime);
     document.querySelector("html").style.background =
       "url('assets/bff.png')";
     return;
